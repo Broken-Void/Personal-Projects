97b4ee0f24b3631693b1c357669ee70967250dc5
Abyss <70314946+Broken-Void@users.noreply.github.com>
2021-09-22 18:53:09 +1200

    Updated code

--- Price Calculator/Shopping_Calculator.py
+++ Price Calculator/Shopping_Calculator.py
@@ -14,12 +14,15 @@ class ShoppingCalculator:
         self.bg_color = '#3a3657'
         self.master.configure(background=self.bg_color)
         self.txt_color = "#9580ff"
-        self.font_txt = 'open sans', 12, ''
-        self.font_entry = 'open sans', 10, ''
+        self.font_txt = 'Cormorant', 12, ''
+        self.font_output = 'Cormorant', '12', 'bold'
+        self.font_entry = 'Cormorant', 10, ''
 
         # Setting colours/styling
         self.Mframe = ttk.Style()
-        self.Mframe.theme_use('clam')
+        # Just simply import the azure.tcl file
+
+        # Then set the theme you want with the set_theme procedure
         self.Mframe.configure('MF.TFrame', background=self.bg_color)
         self.TLabel = ttk.Style()
         self.TLabel.configure('TLabel', background=self.bg_color, foreground=self.txt_color)
@@ -40,10 +43,10 @@ class ShoppingCalculator:
         
         converter = requests.get("https://www.x-rates.com/table/?from=NZD&amount=1") # X-rates is the website in which I source the conversion rates, all rights are theirs.
         soup = bs4.BeautifulSoup(converter.content, 'html.parser')
-
+        
+        # get combox box store in variable. then iterate over it till it finds the right one
         self.USD_rate = soup.find_all('td')[2].text # The [number] is = to the table position
         self.AUD_rate = soup.find_all('td')[14].text
-        return
 
     def price_display(self):
         self.product_cost = tk.IntVar()
@@ -51,38 +54,43 @@ class ShoppingCalculator:
         self.GST = 0.15
 
         lbl_Intro = ttk.Label(self.frm_Price, text="Hello There! \n This little app will allow you to quickly calculate a \n products total cost, GST inclusive! \n Also down below is a USD and AUD converter!",
-        font=('open sans', 13, 'bold'), justify='center', anchor="center", style='TLabel')
+        font=('Cormorant', 13, 'bold'), justify='center', anchor="center", style='TLabel')
         lbl_Intro.grid(row=0, columnspan=3)
 
-        lbl_GST = ttk.Label(self.frm_Price, text="GST used in the calculation is 15%", font=('open sans', 10, 'italic'), anchor="center", style='TLabel')
-        lbl_GST.grid(row=1, columnspan=3, pady=10)
+        sep = ttk.Separator(self.frm_Price, orient='horizontal')
+        sep.grid(row=1, columnspan=3, sticky='NESW')
+
+        lbl_Calculate = ttk.Label(self.frm_Price, text="product calculator:".title(), font=(self.font_output), style='TLabel')
+        lbl_Calculate.grid(row=2, columnspan=3, padx=5, pady=5)
 
         lbl_Product = ttk.Label(self.frm_Price, text="Product Cost:", font=(self.font_txt), style='TLabel')
-        lbl_Product.grid(row=2, column=1, sticky="w", padx=5, pady=10)
+        lbl_Product.grid(row=3, column=1, sticky="w", padx=0, pady=15)
 
         entry_Product = ttk.Entry(self.frm_Price, textvariable=self.product_cost, width=5, font=(self.font_entry))
-        entry_Product.grid(row=2, column=2, sticky="w")
+        entry_Product.grid(row=3, column=1, sticky="e")
         entry_Product.bind("<Return>", lambda event: self.calculate_price())
         entry_Product.focus()
 
         lbl_Shipping = ttk.Label(self.frm_Price, text="Shipping Cost:", font=(self.font_txt), style='TLabel')
-        lbl_Shipping.grid(row=3, column=1, sticky="w", padx=5, pady=10)
+        lbl_Shipping.grid(row=4, column=1, sticky="w", padx=0, pady=10)
 
         entry_Shipping = ttk.Entry(self.frm_Price, textvariable=self.shipping_cost, width=5, font=(self.font_entry))
-        entry_Shipping.grid(row=3, column=2, sticky="w")
+        entry_Shipping.grid(row=4, column=1, sticky="e")
         entry_Shipping.bind("<Return>", lambda event: self.calculate_price())
-    
-        btnCalculate = ttk.Button(self.frm_Price, text="Calculate!", width="20", command=lambda: self.calculate_price())
-        btnCalculate.grid(row=4, columnspan=3, pady=10)
 
+        lbl_GST = ttk.Label(self.frm_Price, text="GST used in the calculation is 15%", font=('Cormorant', 10, 'italic'), anchor="center", style='TLabel')
+        lbl_GST.grid(row=5, columnspan=3, pady=5)
+
+        btnCalculate = ttk.Button(self.frm_Price, text="Calculate!", width="20", command=lambda: self.calculate_price())
+        btnCalculate.grid(row=6, columnspan=3, pady=10)
+        
     def calculate_price(self):
         total_cost = self.product_cost.get() + self.shipping_cost.get()
         total_gst = total_cost * self.GST
         cost_txt = 'Total Cost: ' + '$' + str(round(total_cost + total_gst, 2))
 
-        lbl_Cost = ttk.Label(self.frm_Price, text=cost_txt, font=(self.font_txt), style='TLabel', foreground="red")
-        lbl_Cost.grid(row=5, columnspan=3, pady=10)
-        return
+        lbl_Cost = ttk.Label(self.frm_Price, text=cost_txt, font=(self.font_output), style='TLabel')
+        lbl_Cost.grid(row=7, columnspan=3, pady=10)
 
     def conversion_display(self):
         self.currency = {"Currency": None,
@@ -91,15 +99,21 @@ class ShoppingCalculator:
         self.conversion = tk.StringVar()
         self.conversion_amount = tk.IntVar()
 
+        sep = ttk.Separator(self.frm_Price, orient='horizontal')
+        sep.grid(row=8, columnspan=3, sticky='NESW')
+
+        lbl_Convert = ttk.Label(self.frm_Conversion, text="Convert:", font=(self.font_output), style='TLabel')
+        lbl_Convert.grid(row=8, columnspan=3, padx=5, pady=0)
+
         opt_Conversion = ttk.OptionMenu(self.frm_Conversion, self.conversion, *self.currency)
-        opt_Conversion.grid(row=6, column=0, sticky="e", padx=10, pady=5)
+        opt_Conversion.grid(row=9, column=0, sticky="e", padx=10, pady=5)
 
         entry_Conversion = ttk.Entry(self.frm_Conversion, textvariable=self.conversion_amount, width=5, font=(self.font_entry))
-        entry_Conversion.grid(row=6, column=1, sticky="ew", padx=4)
+        entry_Conversion.grid(row=9, column=1, sticky="ew", padx=4)
         entry_Conversion.bind("<Return>", (lambda event: self.calculate_conversion()))
     
         btnCalculate = ttk.Button(self.frm_Conversion, text="Calculate!", width="20", command=lambda: self.calculate_conversion())
-        btnCalculate.grid(row=7, columnspan=3, pady=10)
+        btnCalculate.grid(row=10, columnspan=3, pady=10)
 
     def calculate_conversion(self):
         new_conversion = self.conversion.get()    
@@ -107,19 +121,16 @@ class ShoppingCalculator:
         try:
             new_cost = new_amount * self.currency[new_conversion]
         except TypeError:
-            lblError = ttk.Label(self.frm_Conversion, text="Please choose a currency".upper(), font=('open sans', '12', 'bold'), style='TLabel', foreground="red")
-            lblError.grid(row=8, columnspan=3, padx=5, pady=5) 
-            self.frm_Price.after(5000, lblError.destroy)
-            return 
+            lbl_Error = ttk.Label(self.frm_Conversion, text="Please choose a currency".upper(), font=(self.font_output), style='TLabel')
+            lbl_Error.grid(row=11, columnspan=3, padx=5, pady=5) 
+            self.frm_Price.after(5000, lbl_Error.destroy)
 
-        lblConversion = ttk.Label(self.frm_Conversion, text="NZD $" + str(round(new_cost,2)).upper(), font=('open sans', '12', 'bold'), style='TLabel', foreground="red")
-        lblConversion.grid(row=8, columnspan=3,  padx=5, pady=5)
-        return
+        lbl_Conversion = ttk.Label(self.frm_Conversion, text="NZD $" + str(round(new_cost,2)).upper(), font=(self.font_output), style='TLabel')
+        lbl_Conversion.grid(row=11, columnspan=3,  padx=5, pady=5)
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(__file__)) # Ensures directory is right so that the icon does not cause issues.
     root = tk.Tk()
     root.title("Shopping Calculator")
-    root.iconbitmap('logo.ico') # The image used is not mine and all rights go to the rightful owner.
     ShoppingCalculator(root)
     root.mainloop()    
